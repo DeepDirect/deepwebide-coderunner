81a08fd8e6508beb9cbb4b6e626f2958cd043b26
projectmiluju <dnjsdyd0712@naver.com>
2025-07-23 01:40:21 +0900

    initialize sandbox-server project with Gradle and Spring Boot setup

---
+++ src/main/java/com/sandbox/sandbox_server/SandboxServerApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.sandbox_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxServerApplication.class, args);
+	}
+
+}
